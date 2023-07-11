c3f65496e5502675a8a550ff9712f8339a94a615
pusuwa <>
2023-07-12 01:06:35 +0300

    Add endpoint for parsing SovetRomantica playlists. Add prettier.

--- app.ts
+++ app.ts
@@ -1,26 +1,62 @@
-import express from 'express';
-import axios from 'axios';
+import express from "express";
+import axios from "axios";
+const m3u8Parser = require("m3u8-parser");
 
 const app = express();
 
-async function parsePlayists(playerUrl: string) {
-    const res = await axios.get<string>(playerUrl);
-    const playlists = res.data.split(',').reduce((acc, el) => {
-        const matches = el.match(/^"url([0-9]+)":/);
-        return matches ? { ...acc, [`mp4_${matches[1]}`]: decodeURI(el.replace(matches[0], '').slice(1, -1)) } : acc;
-    }, {});
-
-    return playlists;
+async function parseVkPlaylists(playerUrl: string) {
+  const res = await axios.get<string>(playerUrl);
+  return res.data.split(",").reduce((acc, el) => {
+    const matches = el.match(/^"url([0-9]+)":/);
+    return matches
+      ? {
+          ...acc,
+          [`mp4_${matches[1]}`]: decodeURI(
+            el.replace(matches[0], "").slice(1, -1),
+          ),
+        }
+      : acc;
+  }, {});
 }
 
-app.get('/api/anime/vk-videos', async (req, res) => {
-    return res.json({
-        response: {
-            items: [{
-                files: await parsePlayists(req.query.playerUrl as string)
-            }]
-        }
-    })
-})
+async function parseSovetRomanticaPlaylists(playlistUrl: string) {
+  const res = await axios.get<string>(playlistUrl);
+  const parser = new m3u8Parser.Parser();
 
-app.listen(8080)
+  parser.push(res.data);
+  parser.end();
+
+  const url = new URL(playlistUrl);
+
+  return parser.manifest.playlists.reduce(
+    (acc, { attributes, uri }) => ({
+      ...acc,
+      [`src_${attributes.RESOLUTION.height}`]: `${url.origin}${url.pathname
+        .split("/")
+        .slice(0, -1)
+        .join("/")}/${uri}`,
+    }),
+    {},
+  );
+}
+
+app.get("/api/anime/vk-videos", async (req, res) => {
+  return res.json({
+    response: {
+      items: [
+        {
+          files: await parseVkPlaylists(req.query.playerUrl as string),
+        },
+      ],
+    },
+  });
+});
+
+app.get("/api/anime/sovetromantica-videos", async (req, res) => {
+  const parsed = await parseSovetRomanticaPlaylists(
+    req.query.playlistUrl as string,
+  );
+  return res.json(parsed);
+});
+
+app.listen(8080);
